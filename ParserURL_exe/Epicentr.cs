163ae64afc99fec59f2fd99a0ec76f6c85ea340a
VitalikZ777 <48833594+VitalikZ777@users.noreply.github.com>
2022-02-17 16:50:16 +0200

    Add files via upload

---
+++ ParserURL_exe/Epicentr.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserURL_dll
+{
+    class Epicentr
+    {
+
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        
+     
+            [JsonProperty("@context")]
+            public string Context { get; set; }
+
+            [JsonProperty("@type")]
+            public string Type { get; set; }
+            public string name { get; set; }
+            public string image { get; set; }
+            public Offers offers { get; set; }
+            public Brand brand { get; set; }
+            public string model { get; set; }
+            public string description { get; set; }
+
+
+        public class Seller
+        {
+            [JsonProperty("@type")]
+            public string Type { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Offers
+        {
+            [JsonProperty("@type")]
+            public string Type { get; set; }
+            public string priceCurrency { get; set; }
+            public string priceValidUntil { get; set; }
+            public string url { get; set; }
+            public double price { get; set; }
+            public string itemCondition { get; set; }
+            public Seller seller { get; set; }
+            public string availability { get; set; }
+        }
+
+        public class Brand
+        {
+            [JsonProperty("@type")]
+            public string Type { get; set; }
+            public string name { get; set; }
+        }
+    }
+    
+
+}
